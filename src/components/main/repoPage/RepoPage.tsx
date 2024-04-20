0137fc036ede43d28f835a1daba3739a45cd6bc0
fultlin <ugolok.kita@hmail.com>
2024-04-20 13:15:55 +0300

    start repopage

--- src/components/main/repoPage/RepoPage.tsx
+++ src/components/main/repoPage/RepoPage.tsx
@@ -3,17 +3,37 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const RepoPage = () => {
-    const { name } = useParams<{ name: string }>(); // Получаем параметр маршрута "name"
-    
+    const { name } = useParams<{ name: string }>();
     const [repoInfo, setRepoInfo] = useState<any>(null);
+    const [lang, setLang] = useState<string | null>('')
+    const [contributors, setContributors] = useState<string | null>('')
+    const [topics, setTopics] = useState<any>(null)
 
     useEffect(() => {
         axios.get(`https://api.github.com/repos/ktsstudio/${name}`).then(response => {
             setRepoInfo(response.data);
+            setTopics(response.data.topics)
         }).catch(error => {
             console.error("Error fetching repository info:", error);
         });
     }, [name]); 
+
+    useEffect(() => {
+        axios.get(`https://api.github.com/repos/ktsstudio/${name}/languages`).then(response => {
+            console.log('lang ', response.data)
+            setLang(response.data)
+        })
+    }, [repoInfo])
+
+    useEffect(() => {
+        axios.get(`https://api.github.com/repos/ktsstudio/${name}/contributors`).then(response => {
+            console.log('contributors', response.data)
+            setContributors(response.data)
+        })
+    }, [repoInfo])
+    
+    console.log('topics', topics)
+    
 
     if (!repoInfo) {
         return <div>Loading...</div>; 
